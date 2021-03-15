e464950b06e4501b049385a0bbb620e027e875e9
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-15 17:46:33 -0400

    commit tree f113ada04e11a1d606b77cc390340b850e3441b4

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  state={
+    text: ''
+  }
+
+  handleOnChange = (e) => {
+    this.setState({text: e.target.value})
+  }
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    this.props.addRestaurant(this.state.text)
+    this.setState({text: ''})
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <label>Name:</label>
+          <input type="text" value={this.state.name} onChange={this.handleOnChange}></input>
+          <input type="submit"></input>
+        </form>
+        <p>{this.state.name}</p>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,13 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = {restaurants: []}, action) {
+  switch(action.type){
+    case 'ADD_RESTAURANT':
+      const rest = {text: action.text, id: cuid()}
+      return {...state, restaurants: [...state.restaurants, rest]}
+    case 'DELETE_RESTAURANT':
+      return {...state, restaurants: state.restaurants.filter(rest => rest.id !== action.id)}
+    default:
+      return state
+  }
 }
